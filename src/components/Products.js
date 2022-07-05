3ef9221772601a7bcbd02c3b9f3d2e545395df06
Murilo Paços <murilopacos@gmail.com>
2022-07-05 09:39:35 -0300

    adicionando Products

---
+++ src/components/Products.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class Products extends Component {
+  render() {
+    const { title, thumbnail, price } = this.props;
+
+    return (
+      <div>
+        <div id="conteiner-product" data-testid="product">
+          <div>
+            <h2>{title}</h2>
+            <img src={ thumbnail } alt={ title } />
+            <p>{price}</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+Products.propTypes = {
+  title: PropTypes.string,
+  thumbnail: PropTypes.string,
+  price: PropTypes.number,
+};
+
+Products.defaultProps = {
+  title: '',
+  thumbnail: '',
+  price: 0,
+};
