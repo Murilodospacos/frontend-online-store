77d03a435d0ecb50b103e7e8999d851b81375141
Murilo Paços <murilopacos@gmail.com>
2022-07-05 09:36:46 -0300

    adicionando rotas e api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Route } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
+import CartItems from './components/CartItems';
+import Details from './components/Details';
+import Home from './components/Home';
+import Checkout from './components/Checkout';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h1>Murilo</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Route exact path="/" component={ Home } />
+      <Route exact path="/cartitems" component={ CartItems } />
+      <Route path="/checkout" render={ (props) => <Checkout { ...props } /> } />
+      <Route path="/details/:id" render={ (props) => <Details { ...props } /> } />
+    </BrowserRouter>
   );
 }
 
---
+++ src/services/api.js
@@ -0,0 +1,11 @@
+export async function getCategories() {
+    const getApi = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+    const resultApi = await getApi.json();
+    return resultApi;
+  }
+  
+  export async function getProductsFromCategoryAndQuery(categoryId, query) {
+    const getApi = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`);
+    const resultApi = await getApi.json();
+    return resultApi;
+  }
