716d6bf972b1c07675504a5dae2736af90a9af93
Murilo Paços <murilopacos@gmail.com>
2022-07-05 09:42:04 -0300

    adicionando category

---
+++ src/components/Category.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class Category extends Component {
+  render() {
+    const { value, name, onClick } = this.props;
+
+    return (
+      <div>
+        <label htmlFor={ value }>
+          <input
+            name="categoria"
+            data-testid="category"
+            type="radio"
+            id={ value }
+            value={ value }
+            onClick={ onClick }
+          />
+          { name }
+        </label>
+      </div>
+    );
+  }
+}
+
+Category.propTypes = {
+  value: PropTypes.string,
+  name: PropTypes.string,
+  onClick: PropTypes.func.isRequired,
+};
+
+Category.defaultProps = {
+  value: '',
+  name: '',
+};
